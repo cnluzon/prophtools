6a6582f6535be73a2c74713f3ced7042f008ffc6
cnluzon <carmennavarro@gmail.com>
2017-08-10 13:36:09 +0200

    Added node attribute parsing to gexf

--- prophtools/test/graphio_test.py
+++ prophtools/test/graphio_test.py
@@ -15,45 +15,221 @@ class TestGraphIOFunctions(unittest.TestCase):
     def setUp(self):
         self.test_dir = tempfile.mkdtemp()
         self.test_file = os.path.join(self.test_dir, 'temp.gexf')
+        self.test_three_group_gexf_file = os.path.join(self.test_dir, 'temp2.gexf')
+        self.test_no_attr_file = os.path.join(self.test_dir, 'no_attribute.gexf')
+
+        self.test_missing_attr_file = os.path.join(self.test_dir, 'missing_attr.gexf')
+        
         self._write_sample_gexf_file(self.test_file)
+        self._write_sample_three_group_gexf_file(self.test_three_group_gexf_file)
+        self._write_gexf_file_no_attributes(self.test_no_attr_file)
+        self._write_gexf_file_missing_attributes(self.test_missing_attr_file)
+
+
+    def _write_gexf_file_missing_attributes(self, filename):
+        value = """<?xml version="1.0" encoding="UTF-8"?>
+<gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">
+<meta lastmodifieddate="2014-01-30">
+<creator>Gephi 0.8.1</creator>
+<description></description>
+</meta>
+<graph defaultedgetype="undirected" mode="static">
+    <attributes class="node">
+        <attribute id="0" title="group" type="integer"/>    
+        <attribute id="1" title="other" type="integer"/>
+    </attributes>
+    <nodes>
+        <node id="0" label="Mark Johnson">
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="1" label="Jane Schwartz">
+            <attvalue for="0" value="0"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="2" label="Ed Lopez">
+            <attvalue for="0" value="1"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="3" label="Maria Lopez">
+            <attvalue for="0" value="1"/>
+            <attvalue for="1" value="0"/>
+        </node>            
+    </nodes>
+
+    <edges>
+        <edge id="0" source="0" target="1" weight="0.25"/>
+        <edge id="1" source="0" target="3" weight="0.88"/>
+        <edge id="2" source="2" target="3" weight="1.00"/>
+        <edge id="3" source="1" target="2" weight="0.52"/>           
+    </edges>
+
+</graph>
+</gexf>
+    """
+        fo = open(filename, 'w')
+        fo.write(value)
+        fo.close()
+
+
+    def _write_gexf_file_no_attributes(self, filename):
+        value = """<?xml version="1.0" encoding="UTF-8"?>
+<gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">
+<meta lastmodifieddate="2014-01-30">
+<creator>Gephi 0.8.1</creator>
+<description></description>
+</meta>
+<graph defaultedgetype="undirected" mode="static">
+    <attributes class="node">
+        <attribute id="1" title="other" type="integer"/>
+        <attribute id="2" title="pepe" type="integer"/>
+    </attributes>
+    <nodes>
+        <node id="0" label="Mark Johnson">
+            <attvalue for="0" value="0"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="1" label="Jane Schwartz">
+            <attvalue for="0" value="0"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="2" label="Ed Lopez">
+            <attvalue for="0" value="1"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="3" label="Maria Lopez">
+            <attvalue for="0" value="1"/>
+            <attvalue for="1" value="0"/>
+        </node>            
+    </nodes>
+
+    <edges>
+        <edge id="0" source="0" target="1" weight="0.25"/>
+        <edge id="1" source="0" target="3" weight="0.88"/>
+        <edge id="2" source="2" target="3" weight="1.00"/>
+        <edge id="3" source="1" target="2" weight="0.52"/>           
+    </edges>
+
+</graph>
+</gexf>
+    """
+        fo = open(filename, 'w')
+        fo.write(value)
+        fo.close()
+
 
     def _write_sample_gexf_file(self, filename):
         value = """<?xml version="1.0" encoding="UTF-8"?>
 <gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">
-    <meta lastmodifieddate="2014-01-30">
-    <creator>Gephi 0.8.1</creator>
-    <description></description>
-    </meta>
-    <graph defaultedgetype="undirected" mode="static">
-        <attributes class="node">
-            <attribute id="0" title="group" type="integer"/>
-        </attributes>
-        <nodes>
-            <node id="0" label="Mark Johnson">
-                <attvalue for="0" value="0"/>
-            </node>
-            <node id="1" label="Jane Schwartz">
-                <attvalue for="0" value="0"/>
-            </node>
-            <node id="2" label="Ed Lopez">
-                <attvalue for="0" value="1"/>
-            </node>
-            <node id="3" label="Maria Lopez">
-                <attvalue for="0" value="0"/>
-            </node>            
-        </nodes>
+<meta lastmodifieddate="2014-01-30">
+<creator>Gephi 0.8.1</creator>
+<description></description>
+</meta>
+<graph defaultedgetype="undirected" mode="static">
+    <attributes class="node">
+        <attribute id="0" title="group" type="integer"/>
+        <attribute id="1" title="other" type="integer"/>
+    </attributes>
+    <nodes>
+        <node id="0" label="Mark Johnson">
+            <attvalue for="0" value="0"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="1" label="Jane Schwartz">
+            <attvalue for="0" value="0"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="2" label="Ed Lopez">
+            <attvalue for="0" value="1"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="3" label="Maria Lopez">
+            <attvalue for="0" value="1"/>
+            <attvalue for="1" value="0"/>
+        </node>            
+    </nodes>
 
-        <edges>
-            <edge id="0" source="0" target="1" weight="0.25"/>
-            <edge id="1" source="0" target="3" weight="0.88"/>
-            <edge id="2" source="2" target="3" weight="1.00"/>
-            <edge id="3" source="1" target="2" weight="0.52"/>           
-        </edges>
+    <edges>
+        <edge id="0" source="0" target="1" weight="0.25"/>
+        <edge id="1" source="0" target="3" weight="0.88"/>
+        <edge id="2" source="2" target="3" weight="1.00"/>
+        <edge id="3" source="1" target="2" weight="0.52"/>           
+    </edges>
 
-    </graph>
+</graph>
 </gexf>
-        """
-        fo = open(self.test_file, 'w')
+    """
+        fo = open(filename, 'w')
+        fo.write(value)
+        fo.close()
+
+
+    def _write_sample_three_group_gexf_file(self, filename):
+        value =  """<?xml version="1.0" encoding="UTF-8"?>
+<gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">
+<meta lastmodifieddate="2014-01-30">
+<creator>Gephi 0.8.1</creator>
+<description></description>
+</meta>
+<graph defaultedgetype="undirected" mode="static">
+    <attributes class="node">
+        <attribute id="0" title="group" type="integer"/>    
+    </attributes>
+    <nodes>
+        <node id="0" label="0_group_0">
+            <attvalue for="0" value="0"/>
+        </node>
+        <node id="1" label="1_group_0">
+            <attvalue for="0" value="0"/>
+        </node>
+        <node id="2" label="2_group_0">
+            <attvalue for="0" value="0"/>
+        </node>
+        <node id="3" label="3_group_1">
+            <attvalue for="0" value="1"/>
+        </node>     
+        <node id="4" label="4_group_1">
+            <attvalue for="0" value="1"/>
+        </node>    
+        <node id="5" label="5_group_2">
+            <attvalue for="0" value="2"/>
+        </node>    
+        <node id="6" label="6_group_2">
+            <attvalue for="0" value="2"/>
+        </node>    
+        <node id="7" label="7_group_2">
+            <attvalue for="0" value="2"/>
+        </node>    
+        <node id="8" label="8_group_2">
+            <attvalue for="0" value="2"/>
+        </node>    
+              
+    </nodes>
+
+    <edges>
+        <!-- Edges within groups -->
+        <edge id="0" source="1" target="2" weight="0.25"/>
+        <edge id="1" source="0" target="2" weight="0.88"/>
+        
+        <edge id="2" source="3" target="4" weight="1.00"/>
+        
+        <edge id="3" source="5" target="7" weight="0.52"/>    
+        <edge id="4" source="7" target="8" weight="0.52"/>
+        <edge id="5" source="6" target="8" weight="0.52"/>
+        
+        <!-- Edges across groups -->
+        <edge id="6" source="0" target="3" weight="1.00"/>
+        <edge id="7" source="2" target="4" weight="1.00"/>
+
+        <edge id="8" source="1" target="7" weight="1.00"/> 
+        <edge id="9" source="4" target="6" weight="1.00"/>
+        <edge id="10" source="4" target="8" weight="1.00"/>
+
+    </edges>
+
+</graph>
+</gexf>
+    """
+        fo = open(filename, 'w')
         fo.write(value)
         fo.close()
 
@@ -104,12 +280,44 @@ class TestGraphIOFunctions(unittest.TestCase):
                 result_j = nodes.index(str(j))
                 self.assertEquals(adj_matrix[i,j], result[result_i,result_j])
 
+    def test_load_graph_gexf_three_adjacency_matrix(self):
+        #                    0     1     2     3     4     5     6     7     8   
+        result = np.matrix([[0.00, 0.00, 0.88, 1.00, 0.00, 0.00, 0.00, 0.00, 0.00], # 0
+                            [0.00, 0.00, 0.25, 0.00, 0.00, 0.00, 0.00, 1.00, 0.00], # 1 
+                            [0.88, 0.25, 0.00, 0.00, 1.00, 0.00, 0.00, 0.00, 0.00], # 2
+                            [1.00, 0.00, 0.00, 0.00, 1.00, 0.00, 0.00, 0.00, 0.00], # 3
+                            [0.00, 0.00, 1.00, 1.00, 0.00, 0.00, 1.00, 0.00, 1.00], # 4
+                            [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.52, 0.00], # 5
+                            [0.00, 0.00, 0.00, 0.00, 1.00, 0.00, 0.00, 0.00, 0.52], # 6
+                            [0.00, 1.00, 0.00, 0.00, 0.00, 0.52, 0.00, 0.00, 0.52], # 7
+                            [0.00, 0.00, 0.00, 0.00, 1.00, 0.00, 0.52, 0.52, 0.00]]) # 8
+
+        graph = graphio.load_graph(self.test_three_group_gexf_file, format='GEXF')
+        nodes = graph.nodes()
+        adj_matrix = nx.adjacency_matrix(graph).todense()
+        
+        for i in range(result.shape[0]):
+            for j in range(result.shape[1]):
+                result_i = nodes.index(str(i))  # Order of the nodes is not fixed
+                result_j = nodes.index(str(j))
+                self.assertEquals(adj_matrix[i,j], result[result_i,result_j])
+
     def test_node_attributes(self):
         graph = graphio.load_graph(self.test_file, format='GEXF')
-        node_attributes = nx.get_node_attributes(graph, "0")
-        print node_attributes
-        self.assertEquals[node_attributes['2']] == '1'
+        self.assertEquals(graph.node['0']['group'], '0')
+        self.assertEquals(graph.node['1']['group'], '0')
+        self.assertEquals(graph.node['2']['group'], '1')
+        self.assertEquals(graph.node['3']['group'], '1')
+
+    def test_file_with_no_group_attribute_raises_exception(self):
+        with self.assertRaises(ValueError):
+            graph = graphio.load_graph(self.test_no_attr_file)
+
+    def test_file_with_missing_group_attributes_raises_exception(self):
+        with self.assertRaises(ValueError):
+            graph = graphio.load_graph(self.test_missing_attr_file)
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestGraphIOFunctions)
     unittest.TextTestRunner(verbosity=2).run(suite)
+    # unittest.main()
--- prophtools/utils/graphio.py
+++ prophtools/utils/graphio.py
@@ -11,6 +11,54 @@ Functions for reading graph files.
 import networkx as nx
 from prophtools.common.graphdata import GraphDataSet
 import simplegexf
+import shutil
+import tempfile
+import os
+
+def _write_sample_gexf_file(filename):
+    value = """<?xml version="1.0" encoding="UTF-8"?>
+<gexf xmlns="http://www.gexf.net/1.2draft" version="1.2">
+<meta lastmodifieddate="2014-01-30">
+<creator>Gephi 0.8.1</creator>
+<description></description>
+</meta>
+<graph defaultedgetype="undirected" mode="static">
+    <attributes class="node">
+        <attribute id="0" title="group" type="integer"/>
+        <attribute id="1" title="other" type="integer"/>
+    </attributes>
+    <nodes>
+        <node id="0" label="Mark Johnson">
+            <attvalue for="0" value="0"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="1" label="Jane Schwartz">
+            <attvalue for="0" value="0"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="2" label="Ed Lopez">
+            <attvalue for="0" value="1"/>
+            <attvalue for="1" value="0"/>
+        </node>
+        <node id="3" label="Maria Lopez">
+            <attvalue for="0" value="1"/>
+            <attvalue for="1" value="0"/>
+        </node>            
+    </nodes>
+
+    <edges>
+        <edge id="0" source="0" target="1" weight="0.25"/>
+        <edge id="1" source="0" target="3" weight="0.88"/>
+        <edge id="2" source="2" target="3" weight="1.00"/>
+        <edge id="3" source="1" target="2" weight="0.52"/>           
+    </edges>
+
+</graph>
+</gexf>
+    """
+    fo = open(filename, 'w')
+    fo.write(value)
+    fo.close()
 
 def validate_allowed_format(f):
     allowed_formats = ['GEXF']
@@ -25,7 +73,7 @@ def load_graph(filename, format="GEXF"):
         raise TypeError(msg)
     
     graph = nx.read_gexf(filename)
-    self.load_node_attributes(graph, filename)
+    load_node_attributes(graph, filename)
     return graph
 
 def load_node_attributes(graph, filename):
@@ -34,4 +82,63 @@ def load_node_attributes(graph, filename):
     for this is that currently networkx does NOT implement node attribute parsing.
     """
     gexf = simplegexf.Gexf(filename)
+    # print dir(gexf)
     
+    attributes_list = gexf.data['graph']['attributes']['attribute']
+    group_attribute_id = None
+
+    att_names = []
+    try:    
+        att_names = [a['@title'] for a in attributes_list]
+    except TypeError:
+        att_names = [attributes_list['@title']]
+
+    if 'group' not in att_names:
+        msg = "Invalid node attributes. These should include a group integer attribute"
+        raise ValueError(msg)
+
+    if not isinstance(attributes_list, list):
+        attributes_list = [attributes_list]
+
+    for a in attributes_list:
+        if a['@title'] == 'group':
+            group_attribute_id = a['@id']
+            if a['@type'] != 'integer':
+                msg = "Invalid troup type attribute. Should be an integer."
+                raise ValueError(msg)
+
+
+    nodes_list = gexf.data['graph']['nodes']['node']
+
+    node_attributes = {}
+    for n in nodes_list:
+        node_id = n['@id']
+        group_value = find_tag_value(n['attvalue'], group_attribute_id)
+        if not group_value:
+            msg = "Found a node with no group attribute {}".format(node_id)
+            raise ValueError(msg)
+
+        node_attributes[node_id] = group_value
+
+    nx.set_node_attributes(graph, 'group', node_attributes)
+    return graph
+
+
+def find_tag_value(att_list, att_id):
+    if not isinstance(att_list, list):
+        att_list = [att_list]
+
+    for attribute in att_list:
+        if attribute['@for'] == att_id:
+           return attribute['@value']
+
+    return None
+
+
+if __name__ == "__main__":
+    test_dir = tempfile.mkdtemp()
+    test_file = os.path.join(test_dir, 'temp.gexf')
+    _write_sample_gexf_file(test_file)
+
+    g = load_graph(test_file)
+    shutil.rmtree(test_dir)
